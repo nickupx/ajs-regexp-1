fd0dd593c09ee3af6659e40e5fefe229cce51179
nickupx <nick.upsky@gmail.com>
2020-09-27 20:57:28 +0300

    1 done

--- src/js/__tests__/validator.test.js
+++ src/js/__tests__/validator.test.js
@@ -1,13 +1,20 @@
 import Validator from '../validator'
 
 test('should be truthy', () => {
-  expect(new Validator('Вася').validateUsername()).toBeTruthy()
+  expect(new Validator('Vasya').validateUsername()).toBeTruthy()
+  expect(new Validator('Va123-sya').validateUsername()).toBeTruthy()
+  expect(new Validator('Va_123-sya').validateUsername()).toBeTruthy()
+  expect(new Validator('Va_123sya').validateUsername()).toBeTruthy()
+  expect(new Validator('V').validateUsername()).toBeTruthy()
+  expect(new Validator('v_ya123v-_va273sya').validateUsername()).toBeTruthy()
 })
 
 test('should be falsy', () => {
-  expect(new Validator('T').validateUsername()).toBeFalsy()
-})
-
-test('should be falsy', () => {
+  expect(new Validator('_Vasya').validateUsername()).toBeFalsy()
+  expect(new Validator('Vas1234ya').validateUsername()).toBeFalsy()
+  expect(new Validator('vasya_').validateUsername()).toBeFalsy()
+  expect(new Validator('1').validateUsername()).toBeFalsy()
+  expect(new Validator('vasya123').validateUsername()).toBeFalsy()
+  expect(new Validator('Vas1234ya122').validateUsername()).toBeFalsy()
   expect(new Validator(23).validateUsername()).toBeFalsy()
 })
--- src/js/validator.js
+++ src/js/validator.js
@@ -4,6 +4,7 @@ export default class Validator {
   }
 
   validateUsername() {
-    return (typeof (this.username) === 'string' && this.username.length > 1)
+    const regexp = new RegExp('^([a-z]{1}|[a-z]+([0-9]{0,3}|[-_]{0,3}|([0-9]{0,3}[-_]{0,3}[a-z]+|[-_]{0,3})*)[a-z]+)$', 'gi')
+    return regexp.test(this.username)
   }
 }
