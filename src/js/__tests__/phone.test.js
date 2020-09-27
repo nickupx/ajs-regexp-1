f30aabecbbcce71b39ad03b9ee7d523a29e7196f
nickupx <nick.upsky@gmail.com>
2020-09-27 22:19:43 +0300

    length checj

--- src/js/__tests__/phone.test.js
+++ src/js/__tests__/phone.test.js
@@ -5,3 +5,7 @@ test('should format', () => {
   expect(formatPhone('+7 960 000 00 00')).toBe('+79600000000')
   expect(formatPhone('+86 000 000 0000')).toBe('+860000000000')
 })
+
+test('should error', () => {
+  expect(() => formatPhone('8 (927) 00-00')).toThrow('Short number')
+})
--- src/js/phone.js
+++ src/js/phone.js
@@ -1,4 +1,7 @@
 export default function formatPhone(number) {
   const result = number.replace(/(-| |\(|\))/g, '').replace(/^8/, '+7')
-  return result
+  if (result.length >= 11) return result
+  throw new Error('Short number')
 }
+
+formatPhone('+7 916 333 22 33')
