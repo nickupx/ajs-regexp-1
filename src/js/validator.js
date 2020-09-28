8c29f559f13cdbf28185481921ea83523af0b9fc
nickupx <nick.upsky@gmail.com>
2020-09-28 11:55:45 +0300

    shorter

--- src/js/validator.js
+++ src/js/validator.js
@@ -1,10 +1,12 @@
+/* eslint-disable max-len */
 export default class Validator {
   constructor(username) {
     this.username = username
   }
 
   validateUsername() {
-    const regexp = new RegExp('^([a-z]{1}|[a-z]+([0-9]{0,3}|[-_]{0,3}|([0-9]{0,3}[-_]{0,3}[a-z]+|[-_]{0,3})*)[a-z]+)$', 'gi')
+    // const regexp = new RegExp('^([a-z]{1}|[a-z]+([0-9]{0,3}|[-_]{0,3}|([0-9]{0,3}[-_]{0,3}[a-z]+|[-_]{0,3})*)[a-z]+)$', 'gi')
+    const regexp = new RegExp('^[a-z](([0-9]{0,3}[a-z_-]+)*[0-9]{0,3}[a-z]+)?$', 'gi') // покороче
     return regexp.test(this.username)
   }
 }
